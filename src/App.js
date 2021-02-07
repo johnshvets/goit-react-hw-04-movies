bb50d0a96a7f9add9341993106159bd978f096e0
johnshvets <johnshvets@gmail.com>
2021-02-07 21:22:18 +0200

    add views components to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AppBar from "./components/AppBar/AppBar";
+import Container from "./components/Container/Container";
+import Spiner from "./components/Spiner/Spiner";
+
+const HomePage = lazy(() =>
+  import("./views/HomePage" /* webpackChunkName: "home-view" */)
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    "./views/MovieDetailsPage" /* webpackChunkName: "movie-details-view" */
+  )
+);
+const MoviesPage = lazy(() =>
+  import("./views/MoviesPage" /* webpackChunkName: "movies-view" */)
+);
+const NotFoundView = lazy(() =>
+  import("./views/NotFoundView" /* webpackChunkName: "not-found-view" */)
+);
+
+const App = () => (
+  <Container>
+    <AppBar />
+
+    <Suspense fallback={<Spiner />}>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+
+        <Route path="/movies" exact>
+          <MoviesPage />
+        </Route>
+
+        <Route path="/movies/:movieId">
+          <MovieDetailsPage />
+        </Route>
+
+        <Route>
+          <NotFoundView />
+        </Route>
+      </Switch>
+    </Suspense>
+  </Container>
+);
 
 export default App;
