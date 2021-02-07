4bd879527e374a53d2d9973a5cc4fab36e0bd2d1
johnshvets <johnshvets@gmail.com>
2021-02-07 21:23:07 +0200

    add AppBar

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,9 @@
+import Navigation from "../Navigation/Navigation";
+import s from "./AppBar.module.css";
+
+const AppBar = () => (
+  <header className={s.header}>
+    <Navigation />
+  </header>
+);
+export default AppBar;
